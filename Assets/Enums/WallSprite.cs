6fd5ba5d8fbcc30d2b8393390ef2936879669d3c
Alexandre Henrique <zaleortgames@outlook.com>
2021-11-07 17:32:06 +0100

    Paredes

---
+++ Assets/Enums/WallSprite.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSprite
+{
+  public static Sprite WallRight = Resources.Load<Sprite>("Sprites/Wall/wall_right");
+  public static Sprite WallLeft = Resources.Load<Sprite>("Sprites/Wall/wall_left");
+  public static Sprite WallUp = Resources.Load<Sprite>("Sprites/Wall/wall_up");
+  public static Sprite CornerDownLeft = Resources.Load<Sprite>("Sprites/Wall/corner_down_left");
+  public static Sprite CornerDownRight = Resources.Load<Sprite>("Sprites/Wall/corner_down_right");
+  public static Sprite CornerUpLeft = Resources.Load<Sprite>("Sprites/Wall/corner_up_left");
+  public static Sprite CornerUpRight = Resources.Load<Sprite>("Sprites/Wall/corner_up_right");
+}
